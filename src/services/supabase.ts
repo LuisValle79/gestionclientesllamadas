871fffb3be032205df52add118885337352d7302
LuisValle79 <luis.valle@vallegrande.edu.pe>
2025-09-08 15:20:01 -0500

    update annon

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -2,9 +2,10 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_SERVICE_ROLE_KEY;
+const supabaseKey_role = import.meta.env.VITE_SUPABASE_SERVICE_ROLE_KEY;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY || ''
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseKey, supabaseKey_role);
 
 // Log para depuración
 console.log('Supabase URL:', supabaseUrl);
